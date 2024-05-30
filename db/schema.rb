1cb030b66fec0393937b7d68d2eaa0d7c3d7a40a
Dave Mateer <davemateer@gmail.com>
2024-05-30 08:12:54 +0100

    auto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_091046) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_070909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_091046) do
   end
 
   create_table "communications", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
