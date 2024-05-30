1021a607dbbf714e873f52a7a82bfc5ec6a8651a
Dave Mateer <davemateer@gmail.com>
2024-05-30 13:22:59 +0100

    auto

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+# pin "@rails/activestorage", to: "activestorage.js"
+# pin "activestorage" # @5.2.8
+pin "@rails/activestorage", to: "activestorage.esm.js"
+
